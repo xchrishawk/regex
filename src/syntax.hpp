9d4c2dca6ae097da5e218b566256d2877c5e66dd
Chris Vig <chris@invictus.so>
2017-01-29 18:57:21 -0600

    Use template for internal syntax nodes.

--- src/syntax.hpp
+++ src/syntax.hpp
@@ -8,6 +8,7 @@
 
 /* -- Includes -- */
 
+#include <array>
 #include <memory>
 
 /* -- Types -- */
@@ -81,106 +82,29 @@ namespace regex
   };
 
   /**
-   * Class representing a concatenation between two subexpressions.
+   * Template for classes representing an internal syntax node.
    */
-  class syntax_concatenation_node : public syntax_node
+  template <regex::syntax_node_type NodeType, size_t NumChildren>
+  class syntax_internal_node : public syntax_node
   {
+
+    /* -- Types -- */
+
+  public:
+
+    /** Unique pointer to a child node. */
+    using child_ptr = std::unique_ptr<const regex::syntax_node>;
+
+    /** Array of children. */
+    using child_array = std::array<const child_ptr, NumChildren>;
 
     /* -- Lifecycle -- */
 
   public:
 
-    /** Constructs a new `regex::syntax_concatenation_node` object with the specified subexpressions. */
-    syntax_concatenation_node(std::unique_ptr<regex::syntax_node> sub_left,
-                              std::unique_ptr<regex::syntax_node> sub_right)
-      : m_sub_left(std::move(sub_left)),
-        m_sub_right(std::move(sub_right))
-    { }
-
-    /* -- Public Methods -- */
-
-    /** Returns the type of this syntax node. */
-    virtual regex::syntax_node_type type() const override
-    {
-      return syntax_node_type::concatenation;
-    }
-
-    /** The sub_left-hand sub-node of this concatenation. */
-    const std::unique_ptr<regex::syntax_node>& sub_left() const
-    {
-      return m_sub_left;
-    }
-
-    /** The sub_right-hand sub-node of this concatenation. */
-    const std::unique_ptr<regex::syntax_node>& sub_right() const
-    {
-      return m_sub_right;
-    }
-
-    /* -- Implementation -- */
-
-  private:
-    std::unique_ptr<regex::syntax_node> m_sub_left;
-    std::unique_ptr<regex::syntax_node> m_sub_right;
-  };
-
-  /**
-   * Class representing a alternation between two subexpressions.
-   */
-  class syntax_alternation_node : public syntax_node
-  {
-
-    /* -- Lifecycle -- */
-
-  public:
-
-    /** Constructs a new `regex::syntax_alternation_node` object with the specified subexpressions. */
-    syntax_alternation_node(std::unique_ptr<regex::syntax_node> sub_left,
-                              std::unique_ptr<regex::syntax_node> sub_right)
-      : m_sub_left(std::move(sub_left)),
-        m_sub_right(std::move(sub_right))
-    { }
-
-    /* -- Public Methods -- */
-
-    /** Returns the type of this syntax node. */
-    virtual regex::syntax_node_type type() const override
-    {
-      return syntax_node_type::alternation;
-    }
-
-    /** The sub_left-hand sub-node of this alternation. */
-    const std::unique_ptr<regex::syntax_node>& sub_left() const
-    {
-      return m_sub_left;
-    }
-
-    /** The sub_right-hand sub-node of this alternation. */
-    const std::unique_ptr<regex::syntax_node>& sub_right() const
-    {
-      return m_sub_right;
-    }
-
-    /* -- Implementation -- */
-
-  private:
-    std::unique_ptr<regex::syntax_node> m_sub_left;
-    std::unique_ptr<regex::syntax_node> m_sub_right;
-  };
-
-  /**
-   * Class representing an optional closure node.
-   */
-  class syntax_optional_node : public syntax_node
-  {
-
-    /* -- Lifecycle -- */
-
-  public:
-
-    /** Constructs a new `regex::syntax_optional_node` object with the specified subexpression. */
-    syntax_optional_node(std::unique_ptr<regex::syntax_node> sub)
-      : m_sub(std::move(sub))
+    /** Constructs a new `regex::syntax_internal_node` with the specified children. */
+    syntax_internal_node(child_array children)
+      : m_children(std::move(children))
     { }
 
     /* -- Public Methods -- */
@@ -190,94 +114,37 @@ namespace regex
     /** Returns the type of this syntax node. */
     virtual regex::syntax_node_type type() const override
     {
-      return syntax_node_type::optional;
+      return NodeType;
     }
 
-    /** Returns the sub-node for this optional closure node. */
-    const std::unique_ptr<regex::syntax_node>& sub() const
+    /** Returns the child nodes of this syntax node. */
+    const child_array& children() const
     {
-      return m_sub;
+      return m_children;
     }
 
     /* -- Implementation -- */
 
   private:
-    std::unique_ptr<regex::syntax_node> m_sub;
+
+    child_array m_children;
+
   };
 
-  /**
-   * Class representing a Kleene closure node.
-   */
-  class syntax_kleene_node : public syntax_node
-  {
+  /** Class for a syntax node representing a concatenation between two subexpressions. */
+  using syntax_concatenation_node = regex::syntax_internal_node<regex::syntax_node_type::concatenation, 2>;
 
-    /* -- Lifecycle -- */
+  /** Class for a syntax node representing a union between two subexpressions. */
+  using syntax_alternation_node = regex::syntax_internal_node<regex::syntax_node_type::alternation, 2>;
 
-  public:
+  /** Class representing an optional closure over a subexpression. */
+  using syntax_optional_node = regex::syntax_internal_node<regex::syntax_node_type::optional, 1>;
 
-    /** Constructs a new `regex::syntax_kleene_node` object with the specified subexpression. */
-    syntax_kleene_node(std::unique_ptr<regex::syntax_node> sub)
-      : m_sub(std::move(sub))
-    { }
+  /** Class representing a Kleene closure over a subexpression. */
+  using syntax_kleene_node = regex::syntax_internal_node<regex::syntax_node_type::kleene, 1>;
 
-    /* -- Public Methods -- */
-
-  public:
-
-    /** Returns the type of this syntax node. */
-    virtual regex::syntax_node_type type() const override
-    {
-      return syntax_node_type::kleene;
-    }
-
-    /** Returns the sub-node for this Kleene closure node. */
-    const std::unique_ptr<regex::syntax_node>& sub() const
-    {
-      return m_sub;
-    }
-
-    /* -- Implementation -- */
-
-  private:
-    std::unique_ptr<regex::syntax_node> m_sub;
-  };
-
-  /**
-   * Class representing a repeat closure node.
-   */
-  class syntax_repeat_node : public syntax_node
-  {
-
-    /* -- Lifecycle -- */
-
-  public:
-
-    /** Constructs a new `regex::syntax_repeat_node` object with the specified subexpression. */
-    syntax_repeat_node(std::unique_ptr<regex::syntax_node> sub)
-      : m_sub(std::move(sub))
-    { }
-
-    /* -- Public Methods -- */
-
-  public:
-
-    /** Returns the type of this syntax node. */
-    virtual regex::syntax_node_type type() const override
-    {
-      return syntax_node_type::repeat;
-    }
-
-    /** Returns the sub-node for this repeat closure node. */
-    const std::unique_ptr<regex::syntax_node>& sub() const
-    {
-      return m_sub;
-    }
-
-    /* -- Implementation -- */
-
-  private:
-    std::unique_ptr<regex::syntax_node> m_sub;
-  };
+  /** Class representing a repeat closure over a subexpression. */
+  using syntax_repeat_node = regex::syntax_internal_node<regex::syntax_node_type::repeat, 1>;
 
 }
 
